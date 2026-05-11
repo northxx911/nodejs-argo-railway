e4128c01231df3baf5846ff86fdac403116a2ac0
northxx911 <northeastcn911@gmail.com>
2026-05-11 18:52:06 +0800

    Update 原来index.js

--- 原来index.js
+++ 原来index.js
@@ -17,8 +17,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'jiedian.jishux.cc.cd';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZTk1MWM4MTE5NGY4YjUxYjYxYWNhMThlOTJlOWU0YzQiLCJ0IjoiNzQ3ZDAyMDQtYzY5My00ODExLWI5YjYtYWU4ZmQ3MjgyOWFkIiwicyI6Ill6WmlaR1kyTmpFdFlqWm1aaTAwTnpkaExXSTVOVFl0Wm1KbFltWXdPVEZsTmpCaSJ9
-c7441873-a934-4f2f-873e-7b02da4234a0';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZTk1MWM4MTE5NGY4YjUxYjYxYWNhMThlOTJlOWU0YzQiLCJ0IjoiNzQ3ZDAyMDQtYzY5My00ODExLWI5YjYtYWU4ZmQ3MjgyOWFkIiwicyI6Ill6WmlaR1kyTmpFdFlqWm1aaTAwTnpkaExXSTVOVFl0Wm1KbFltWXdPVEZsTmpCaSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8080;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
